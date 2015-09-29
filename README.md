c38ab7a056163fac2ffb39cddf189be62fe07f5e
Daniel Rasmuson <dan123911@gmail.com>
2015-09-29 15:13:46 -0700

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a utility to turn [CodeBox](http://www.codeboxapp.com/) Snippets Into Files
+
+## Steps
+
+1. Make sure node is installed
+2. run `git clone github.com/danielrasmuson/CodeboxApp-Export`
+3. run `npm install`
+4. run `node codeBoxToFiles.js`
+5. when prompted pass the file path to your codebox export file
+6. folder "snippets" should appear in same folder as this file
---
+++ codeBoxToFiles.js
@@ -0,0 +1,64 @@
+var fs = require('fs'),
+    xml2js = require('xml2js'),
+    Rx = require('rx'),
+    atob = require('atob'),
+    fileManager = require('easy-file'),
+    readlineSync = require('readline-sync');
+   
+function CodeBoxFiles(filePath){
+  return Rx.Observable.create(function(observer){
+    var parser = new xml2js.Parser();
+    fs.readFile(filePath, function(err, data) {
+      if (err){
+        console.log(err);
+        observer.onError('Invalid Path: '+filePath)
+        return;
+      }
+      parser.parseString(data, function (err, result) {
+        var fileNames = result.database.object.filter(function(obj){
+          return obj.$.type == 'SNIPPET';
+        }).map(function(obj){
+          return {
+            name: obj.attribute[0]._,
+            id: obj.relationship[1].$.idrefs
+          }
+        });
+
+        var files = result.database.object.filter(function(obj){
+          return obj.$.type == 'ASSET';
+        }).map(function(snippet){
+          return fileNames.filter(function(file){
+            return file.id == snippet.$.id;
+          }).map(function(fileData){
+            var body = snippet.attribute[0]._;
+            if (body == -1){
+              if (snippet.attribute[2]){
+                body = snippet.attribute[2]._;
+              } else{
+                console.log('Error: Empty Snippet '+fileData.name);
+              }
+            }
+            return {
+              name: fileData.name,
+              body: atob(body)
+            }
+          })
+        }).reduce(function(a, b){
+          return a.concat(b);
+        })
+
+        files.forEach(function(file){
+          observer.onNext(file)
+        })
+        observer.onCompleted();
+      });
+    });
+  });
+} 
+
+var filePath = readlineSync.question('What is the path to "CodeBox.cbxml"?: ');
+CodeBoxFiles(filePath).forEach(function(file){
+  fileManager.write('CodeBoxSnippets/'+file.name+'.txt', file.body);
+}, function(err){
+  console.log('Error: ', err);
+});
